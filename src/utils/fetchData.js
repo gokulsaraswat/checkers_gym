045a8f6280b21f8b89936a8f632420a030b52fb7
gokul saraswat <gokulsaraswat07@gmail.com>
2022-09-20 20:27:13 +0530

    Updated Website with working functinalities

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,5 +1,6 @@
 export const exerciseOptions = {
   method: 'GET',
+  // url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
   headers: {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
@@ -8,9 +9,11 @@ export const exerciseOptions = {
 
 export const youtubeOptions = {
   method: 'GET',
+  // url: 'https://youtube-search-and-download.p.rapidapi.com/channel/about',
+  // params: {id: 'UCE_M8A5yxnLfW0KghEeajjw'},
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
   },
 };
 
